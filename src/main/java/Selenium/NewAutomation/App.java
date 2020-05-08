2a684e0c724cdc219058086549bb464d7fc7f962
Lakshan <lakshancs1987@gmail.com>
2020-05-08 15:36:40 +0530

    Forth commit

--- src/main/java/Selenium/NewAutomation/App.java
+++ src/main/java/Selenium/NewAutomation/App.java
@@ -13,5 +13,7 @@ public class App
         System.out.println( "Hello World!2" );
         System.out.println( "Hello World!3" );
         System.out.println( "Hello World!4" );
+        System.out.println( "Hello World!5" );
+        System.out.println( "Hello World!6" );
     }
 }
